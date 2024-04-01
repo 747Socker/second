45dde54801d823e17e046ded083907c4d6852dcd
이혜지 <lee02g29@naver.com>
2024-04-01 15:26:57 +0900

    Feat : 생성 중 버튼 클릭 차단, 사용자 커스터마이징 복구 기능, 3회 이상의 재생성 차단, 최초 생성시 빈 이미지

--- frontend/src/containers/generatePage/GeneratePage.tsx
+++ frontend/src/containers/generatePage/GeneratePage.tsx
@@ -5,10 +5,12 @@ import { useEffect, useState } from 'react';
 import { MakeModal } from '../../components/modal/makeModal/MakeModal';
 import { FlowerListModal } from '../../components/modal/flowerModal/FlowerListModal';
 import { bouquetStore } from '../../stores/bouquetStore';
+import { postRegenerateInputs } from '../../api/bouquetReCreate.ts';
+import { useLocalAxios } from '../../utils/axios.ts';
 import CustomButton from '../../components/button/CustomButton';
-import { postRegenerateInputs } from '../../api/bouquetReCreate.ts'
-import setupSSE from "../../utils/sse.ts";
-import {useLocalAxios} from "../../utils/axios.ts";
+import setupSSE from '../../utils/sse.ts';
+import white from '../../assets/images/white.jpg';
+
 type FlowerDto = {
 	flowerId: number;
 	name: string;
@@ -18,10 +20,10 @@ type FlowerDto = {
 };
 
 export const GeneratePage = () => {
-	const {bouquetUrl, usedFlower, recommendByMeaning, allFlowers, setBouquetData, setBouquetUrl } = bouquetStore();
+	const { bouquetUrl, usedFlower, recommendByMeaning, allFlowers, setBouquetData, setBouquetUrl } = bouquetStore();
 	const [isMakeModalOpened, setIsMakeModalOpened] = useState(false);
 	const [isListModalOpened, setIsListModalOpened] = useState(false);
-	
+
 	const [isLoading, setIsLoading] = useState(true);
 	const axiosInstance = useLocalAxios(true);
 	// 확인 모달, 꽃 전체 리스트 모달 여부
@@ -34,6 +36,8 @@ export const GeneratePage = () => {
 	const [userSelectIndex, setUserSelectIndex] = useState<number>(-1);
 	const [userSelectId, setUserSelectId] = useState<number>(-1);
 	const [isUsed, setIsUsed] = useState<boolean[]>([true, true, true]);
+	const [isMaking, setIsMaking] = useState<boolean>(false);
+	const [regeneCounter, setRegeneCounter] = useState<number>(0);
 	// 유저가 선택한 각 꽃 별 추가 꽃
 	// 선택한 꽃의 위치(0, 1, 2)
 	// 유저가 선택한 꽃의 id
@@ -43,6 +47,7 @@ export const GeneratePage = () => {
 	useEffect(() => {
 		setupSSE({
 			onOpen: () => {
+				setIsMaking(true);
 				console.log('SSE 연결이 열림');
 			},
 			onError: (error) => {
@@ -52,19 +57,21 @@ export const GeneratePage = () => {
 				firstGenerateEvent: (data) => {
 					setBouquetData(data);
 					console.log('첫 번째 생성 이벤트 데이터 처리', data);
+					setIsMaking(false);
 				},
 				reGenerateEvent: (data) => {
 					setBouquetData(data);
 					console.log('재생성 이벤트 데이터 처리', data);
+					setIsMaking(false);
 				},
 				middleImageSendEvent: (data) => {
-					setBouquetUrl(data)
-					// console.log(data)
-				}
-			}
+					setBouquetUrl(data);
+				},
+			},
 		});
 
 		return () => {
+			setIsMaking(false);
 		};
 	}, []);
 
@@ -79,7 +86,7 @@ export const GeneratePage = () => {
 		setIsUsed(Array.from({ length: usedFlower.length }, () => true));
 
 		return unsubscribe;
-	},[usedFlower])
+	}, [usedFlower]);
 
 	useEffect(() => {
 		const extractFlower = usedFlowerIndexs
@@ -96,7 +103,7 @@ export const GeneratePage = () => {
 			})
 			.filter((flower) => flower !== undefined) as FlowerDto[];
 
-		setFlowersByMeaning(extractByMeaning)
+		setFlowersByMeaning(extractByMeaning);
 	}, [usedFlowerIndexs]);
 	// 꽃말로 추천할 목록 추출
 
@@ -146,6 +153,10 @@ export const GeneratePage = () => {
 	}; // 추가하려는 꽃의 id
 
 	const handleSubmit = async () => {
+		setIsMaking(true);
+		if(regeneCounter === 2) alert('마지막 재생성입니다.')
+		setRegeneCounter(regeneCounter + 1);
+
 		const inputs = usedFlowerIndexs
 			.map((index, i) => {
 				const flower = allFlowers.find((flower) => flower.flowerId === index);
@@ -154,7 +165,7 @@ export const GeneratePage = () => {
 			.filter((name) => name !== undefined) as string[];
 		// 사용한 꽃 이름만 추출
 
-		await postRegenerateInputs(inputs,axiosInstance);
+		await postRegenerateInputs(inputs, axiosInstance);
 	};
 
 	const setUsedState = (index: number, state: boolean) => {
@@ -163,12 +174,23 @@ export const GeneratePage = () => {
 		setIsUsed(newState.map((state) => state));
 	}; // 꽃의 사용 여부를 체크 -> 삭제한 경우에는 추출하지 않기 위함
 
+	const deleteAddedFlower = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, index: number) => {
+		const newState = [...selectIdByIndex];
+		newState[index] = -1;
+		setSelectIdByIndex(newState.map((state) => state));
+		e.stopPropagation();
+	};
+
+	const onErrorImg = (e: any) => {
+		e.target.src = white;
+	};
+
 	return (
 		<>
 			<StyledGeneratePage>
 				{/* 로그인 헤더 */}
 				<Header></Header>
-				<StyledBouquetImage src={bouquetUrl} alt='img'></StyledBouquetImage>
+				<StyledBouquetImage src={bouquetUrl} onError={onErrorImg}></StyledBouquetImage>
 				{/* 최초 추천 꽃 + 변경 추천 꽃 */}
 				{uf.map((item, index) => {
 					return (
@@ -185,15 +207,23 @@ export const GeneratePage = () => {
 							openListModal={(e) => openListModal(e, index)}
 							changeFlower={changeFlower}
 							setUsedState={setUsedState}
+							deleteAddedFlower={(e) => deleteAddedFlower(e, index)}
 						></Accordion>
 					);
 				})}
-				<div style={{ marginBottom: '2vh' }}>
-					<CustomButton $check={true} onClick={openModal}>
-						확인
-					</CustomButton>
-					<CustomButton onClick={handleSubmit}>재생성</CustomButton>
-				</div>
+				{!isMaking && (
+					<div style={{ marginBottom: '2vh' }}>
+						<CustomButton $check={true} onClick={openModal}>
+							확인
+						</CustomButton>
+						{regeneCounter <= 3 && <CustomButton onClick={handleSubmit}>재생성</CustomButton>}
+					</div>
+				)}
+				{isMaking && (
+					<div style={{ marginBottom: '2vh' }}>
+						<CustomButton $check={true}>생성 중</CustomButton>
+					</div>
+				)}
 			</StyledGeneratePage>
 
 			{/* 완성 확인 모달 */}
--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -11,30 +11,30 @@ const useLocalAxios = (isAuth: boolean = true): AxiosInstance => {
         withCredentials: true,
     });
 
-    if (isAuth) {
-        instance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-            if (!accessToken) {
-                console.log("로그인이 필요합니다.");
-                throw new Axios.Cancel("로그인이 필요한 요청입니다.");
-            }
-            config.headers = config.headers || {};
-            config.headers.Authorization = `Bearer ${accessToken}`;
-            return config;
-        });
-    }
+    // if (isAuth) {
+    //     instance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+    //         if (!accessToken) {
+    //             console.log("로그인이 필요합니다.");
+    //             throw new Axios.Cancel("로그인이 필요한 요청입니다.");
+    //         }
+    //         config.headers = config.headers || {};
+    //         config.headers.Authorization = `Bearer ${accessToken}`;
+    //         return config;
+    //     });
+    // }
 
-    instance.interceptors.response.use(
-        (response) => response,
-        async (error) => {
-            if (error.response && error.response.status === 401) {
-                // 401 Unauthorized 오류 처리
-                clearAuth();
-                navigate('/');
-                return Promise.reject(error);
-            }
-            return Promise.reject(error);
-        },
-    );
+    // instance.interceptors.response.use(
+    //     (response) => response,
+    //     async (error) => {
+    //         if (error.response && error.response.status === 401) {
+    //             // 401 Unauthorized 오류 처리
+    //             clearAuth();
+    //             navigate('/');
+    //             return Promise.reject(error);
+    //         }
+    //         return Promise.reject(error);
+    //     },
+    // );
 
     return instance;
 };
